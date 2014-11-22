c208194b7ed8a9d53b517f2d7fdbf37e54cc02a1
Simeon Kakpovi <skakpovi@gmail.com>
2014-11-22 22:31:32 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   get 'pages/contact'
+  
   get 'seller' => "listings#seller"
 
   root 'listings#index'
   
-  post 'twilio/voice' => 'twilio#voice'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
