ec403842c94b179ef425aa908d18f7d0f0eb1fcc
Simeon Kakpovi <skakpovi@gmail.com>
2014-12-16 19:44:29 +0000

    added amazon links

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   
   def search
-      @listings = Listing.search(params[:q]).records.where(is_active: true).paginate(:page => params[:page], :per_page => 16)
+      @listings = Listing.search(params[:q]).records.where(is_active: true).paginate(:page => params[:page], :per_page => 16).order(:amazon).order(:name)
   
 end
   
