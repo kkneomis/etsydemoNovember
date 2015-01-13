92bee733f3f280fd9c87e6a2010791a40666c4a1
Simeon Kakpovi <skakpovi@gmail.com>
2015-01-13 04:42:12 +0000

    redo

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   
   def search
-    @listings = Listing.all.where(is_active: true).where(amazon: false).order("created_at DESC")
+    @listings = Listing.all.where(is_active: true).where(amazon: false).order("created_at DESC").paginate(:page => params[:page], :per_page => 12)
 end
   
 end
