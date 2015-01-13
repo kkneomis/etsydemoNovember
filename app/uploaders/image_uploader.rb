71674dc4aa5b0b473c0bd049b99b44faa9c20356
Simeon Kakpovi <skakpovi@gmail.com>
2015-01-13 21:29:25 +0000

    default

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,4 +13,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [240, 320, :face]
   end
 
+   def default_url 
+     "http://res.cloudinary.com/bisonshare/image/upload/v1421184236/bisonshare_o5kfr1.jpg"
+   end
+  
 end
